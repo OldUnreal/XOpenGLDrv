d405431eb4dab0278fd436249a859b001ea2ad4f
Stijn Volckaert <stijnv@gmail.com>
2023-05-02 10:16:21 +0200

    Cleanup and moved header

--- CopyShaders.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-REM The Visual Studio project automatically runs this script after building
-
-if not exist ..\..\System\xopengl mkdir ..\..\System\xopengl
-copy /Y ..\Shaders\*.* ..\..\System\xopengl\
-copy /Y ..\XOpenGLDrv.int ..\..\System\
--- CopyShaders64.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-REM The Visual Studio project automatically runs this script after building
-
-if not exist ..\..\System64\xopengl mkdir ..\..\System64\xopengl
-copy /Y ..\Shaders\*.* ..\..\System64\xopengl\
-copy /Y ..\XOpenGLDrv.int ..\..\System64\
